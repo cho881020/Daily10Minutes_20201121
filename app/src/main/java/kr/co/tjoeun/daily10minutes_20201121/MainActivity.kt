4e3d4b505c5784c3df4af6cdcb7557fc8fcea02a
조경진 <kj_cho@idealidea.co.kr>
2020-11-21 15:45:23 +0900

    우선 버튼 눌리면 => 입력한 아이디/비번 추출까지만

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20201121/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20201121/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.co.tjoeun.daily10minutes_20201121
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
@@ -13,6 +14,20 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        signInBtn.setOnClickListener {
+
+//            1. 입력된 아이디 / 비번 받아오기
+
+            val inputEmail = emailEdt.text.toString()
+            val inputPw = pwEdt.text.toString()
+
+//            2. 서버에 맞는 회원인지 확인 => 로그인 API 호출 => 서버 응답 분석 => UI 반영
+
+
+
+
+        }
 
     }
 
