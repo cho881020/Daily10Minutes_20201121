0aecf47896358ebea76d4cf9b6df7d9c1ea21129
조경진 <kj_cho@idealidea.co.kr>
2020-11-21 15:58:51 +0900

    ServerUtil 클래스 + BASE_URL 변수만 우선 추가

---
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20201121/utils/ServerUtil.kt
@@ -0,0 +1,15 @@
+package kr.co.tjoeun.daily10minutes_20201121.utils
+
+class ServerUtil {
+
+//    ServerUtil.함수() 처럼, 클래스이름.만 해도 기능을 사용하게 도와주는 코드
+    companion object {
+
+//        서버 호스트 주소를 쉽게 입력하기 위한 변수
+        val BASE_URL = "http://15.164.153.174/"
+
+
+    }
+
+
+}
