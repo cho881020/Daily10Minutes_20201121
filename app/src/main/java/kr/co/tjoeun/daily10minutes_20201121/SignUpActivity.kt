7655adf6f7db49536ce7eb9cbdd768227dafc102
조경진 <kj_cho@idealidea.co.kr>
2020-11-22 14:37:15 +0900

    텍스트 내용 변경 이벤트 다른 방법 예시

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20201121/SignUpActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20201121/SignUpActivity.kt
@@ -2,6 +2,8 @@ package kr.co.tjoeun.daily10minutes_20201121
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
@@ -32,6 +34,21 @@ class SignUpActivity : BaseActivity() {
             checkResultTxt.text = "중복 확인을 해주세요."
 
         }
+
+//        idEdt.addTextChangedListener(object : TextWatcher {
+//            override fun afterTextChanged(s: Editable?) {
+//                Log.d("변경된문구", s!!.toString())
+//            }
+//
+//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+//
+//            }
+//
+//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+//
+//            }
+//
+//        })
 
         emailCheckBtn.setOnClickListener {
 
