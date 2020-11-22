aefb8478552aa3752df76dddff61316a661359eb
조경진 <kj_cho@idealidea.co.kr>
2020-11-22 12:41:15 +0900

    실제 회원가입 API 호출  - 다녀와서 code값에 따른 분기 처리 필요

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20201121/SignUpActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20201121/SignUpActivity.kt
@@ -3,6 +3,8 @@ package kr.co.tjoeun.daily10minutes_20201121
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_sign_up.*
+import kr.co.tjoeun.daily10minutes_20201121.utils.ServerUtil
+import org.json.JSONObject
 
 class SignUpActivity : BaseActivity() {
 
@@ -24,9 +26,18 @@ class SignUpActivity : BaseActivity() {
             
 //            2. ServerUtil 활용 => 회원가입 API 호출
 //            회원가입 기능을 ServerUtil에 우선 추가 => 화면에서 활용
+            ServerUtil.putRequestSignUp(mContext, inputId, inputPw, inputNickName, object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(json: JSONObject) {
+
+//                    3. 돌아와서 어떡할지? 코딩
+
+
+
+                }
+
+            })
             
-            
-//            3. 돌아와서 어떡할지? 코딩
+
         }
 
     }
