4abe5398181ab653ca20bb6610985e6b8d6b9701
조경진 <kj_cho@idealidea.co.kr>
2020-11-22 16:21:45 +0900

    임시버튼으로 => 로그아웃 기능 구현  - 서버통신 X, 저장된 토큰만 초기화 ("" 으로 설정)

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20201121/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20201121/MainActivity.kt
@@ -1,7 +1,12 @@
 package kr.co.tjoeun.daily10minutes_20201121
 
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.tjoeun.daily10minutes_20201121.utils.ContextUtil
 
 class MainActivity : BaseActivity() {
 
@@ -13,6 +18,26 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        logoutBtn.setOnClickListener {
+//            로그인 : 서버에 아이디 비번이 맞는지? 물어보는 (API 통신) => 맞으면 토큰을 기기에 저장
+//            로그아웃 : 저장된 토큰을 날려주는 행위 => 저장된 토큰값을 "" 로 변경
+
+            val alert = AlertDialog.Builder(mContext)
+            alert.setMessage("정말 로그아웃 하시겠습니까?")
+            alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialog, which ->
+//                저장 토큰 날려주기
+                ContextUtil.setLoginUserToken(mContext, "")
+
+//                메인화면 종료, Splash화면으로 넘어가기
+                val myIntent = Intent(mContext, SplashActivity::class.java)
+                startActivity(myIntent)
+                finish()
+            })
+            alert.setNegativeButton("취소", null)
+            alert.show()
+
+        }
 
     }
 
