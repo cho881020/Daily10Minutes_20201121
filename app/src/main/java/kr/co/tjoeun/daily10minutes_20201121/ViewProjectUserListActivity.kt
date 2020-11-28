34d29957929aefb94cce8a2e5ce6e22d34d7d29b
조경진 <kj_cho@idealidea.co.kr>
2020-11-28 14:49:18 +0900

    참여중인 사용자 명단 보는 화면 추가

---
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20201121/ViewProjectUserListActivity.kt
@@ -0,0 +1,23 @@
+package kr.co.tjoeun.daily10minutes_20201121
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewProjectUserListActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_project_user_list)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
