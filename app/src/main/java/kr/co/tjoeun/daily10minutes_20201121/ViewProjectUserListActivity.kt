969edd1d59b9e330259562515a387cb85d9ade40
조경진 <kj_cho@idealidea.co.kr>
2020-11-28 15:37:19 +0900

    사용자 명단 배열까지 분석 => 이 내부를 반복적으로 파싱할 준비작업

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20201121/ViewProjectUserListActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20201121/ViewProjectUserListActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import kr.co.tjoeun.daily10minutes_20201121.datas.Project
 import kr.co.tjoeun.daily10minutes_20201121.datas.User
 import kr.co.tjoeun.daily10minutes_20201121.utils.ServerUtil
+import org.json.JSONObject
 
 class ViewProjectUserListActivity : BaseActivity() {
 
@@ -35,7 +36,28 @@ class ViewProjectUserListActivity : BaseActivity() {
 
 //        실제로 서버에 접근 (ServerUtil) => 사용자 목록 받아오기. => 내용 분석 + mUserList에 담아주기
 
-//        ServerUtil.getpro
+        ServerUtil.getRequestProjectUserList(mContext, mProject.id, object : ServerUtil.JsonResponseHandler {
+            override fun onResponse(json: JSONObject) {
+
+                val dataObj = json.getJSONObject("data")
+                val projectObj = dataObj.getJSONObject("project")
+
+//                project {} 내부에 있는, ongoing_users [] 를 가져다 분석하자.
+                val ongoingUsersArr = projectObj.getJSONArray("ongoing_users")
+
+//                for문 이용 - 0 ~ 들어있는 사용자 수 직전까지 돌자.
+//                14명 : 0 ~ 13까지 돌자. (현재 몇바퀴째인지? i로 표현하자)
+//                몇명이 들어있는지? ongoingUsersArr 의 기능중, length() 를 실행하면 알려줌.
+
+                for (i in   0 until ongoingUsersArr.length()) {
+
+                }
+
+
+
+            }
+
+        })
 
     }
 
