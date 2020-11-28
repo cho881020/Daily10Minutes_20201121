b9701f32d885a12187bfdf2b924b741d3b97b4fc
조경진 <kj_cho@idealidea.co.kr>
2020-11-28 10:32:16 +0900

    프로젝트 표현용 데이터클래스 생성   - API 테스트 결과를 보고, 그 내용을 담기 위해 설계

---
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20201121/datas/Project.kt
@@ -0,0 +1,13 @@
+package kr.co.tjoeun.daily10minutes_20201121.datas
+
+class Project {
+
+    var id = 0 // id에는 정수가 들어올거라고 명시
+    var title = "" // title에는 String이 들어올거라고 명시
+    var imageURL = ""
+    var description = ""
+    var proofMethod = ""
+    var completeDays = 0
+    var onGoingUsersCount = 0
+
+}
