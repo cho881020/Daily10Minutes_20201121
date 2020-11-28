5e9ee8fc5140556a0166cd9e22baa9c624e3919b
조경진 <kj_cho@idealidea.co.kr>
2020-11-28 15:41:19 +0900

    (알맞는) JSON => User 변환 기능 추가

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20201121/datas/User.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20201121/datas/User.kt
@@ -1,5 +1,7 @@
 package kr.co.tjoeun.daily10minutes_20201121.datas
 
+import org.json.JSONObject
+
 class User {
 
     var id = 0 // id가 Int라고 명시
@@ -9,5 +11,24 @@ class User {
 //    응용
 //    1. 가입일시 (created_at) : 서버는 String으로 알려줌 -> 앱에서는 Calendar 형태로 변환해서 사용.
 //    2. 프로필 사진"들" : 한명의 User가 -> 여러장의 사진URL (String) 을 갖고 있음을 어떻게 표현?
+
+//    JSON 넣으면 => User로 변환해주는 기능 제작
+
+    companion object {
+
+        fun getUserFromJSON(json: JSONObject) : User {
+            
+            val u = User()
+            
+//            u의 변수들을 => json이 가진 값을 활용해서 변경
+
+            u.id = json.getInt("id")
+            u.email = json.getString("email")
+            u.nickName = json.getString("nick_name")
+
+            return u
+        }
+
+    }
 
 }
